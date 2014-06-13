46636e73c8b5fabcea1c91765705c311ff81c9dc
Kubikx <hp.kuba@atlas.cz>
2014-06-13 12:47:50 +0200

    test
    Change-Id: I7d67594d2411cbb5c7ec0aa53dd3e1599d213795

--- src/test/Pokus.java
+++ src/test/Pokus.java
@@ -1,14 +1,34 @@
 package test;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+
+
+
+
 public class Pokus {
 
 	public Pokus() {
-		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("pokus");
-	}
+	public static void main(String[] args) throws LWJGLException {
+		Display.create();
+		Display.setLocation(0, 0);
+		Display.setFullscreen(true);
+		
+		
+		
+		
+		
+		
+//		---------------------------
+		for(int i = 0;i>=0;i++){
 
+//			for (int j = 0;j>=0;j++){
+				float x = i * i;
+//			}
+		}
+//		---------------------------
+	}
 }
